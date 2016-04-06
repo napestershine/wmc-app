115cb52ffb646bae0b2b0fc840ebb67bc09994b8
kneelb4darth <kneelb4darth@gmail.com>
2016-04-06 23:40:49 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# wmc-app
+A new Symfony project started on 5/4/2016
---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: playroom
+ * Date: 6/4/16
+ * Time: 9:04 PM
+ */
+
+
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
